41809167027ce714a11f4604206c09bf6d2af8d3
Ethan <48872919+egrayum@users.noreply.github.com>
2020-05-21 16:50:25 -0600

    Update testGame.js

--- docs/testGame.js
+++ docs/testGame.js
@@ -10,6 +10,10 @@ function init() {
  c.height = 600;
  ctx.fillStyle = "#666";
  ctx.fillRect(0, 0, c.width, c.height);
+ ctx.strokeStyle = "#000";
+ ctx.lineWidth = 10;
+ ctx.moveTo(50, 100);
+ ctx.lineTo(750, 100);
  drawWorld();
 }
 function sq(X, Y, Length, Color) {
@@ -17,7 +21,7 @@ function sq(X, Y, Length, Color) {
  ctx.fillRect(X, Y, Length, Length);
 }
 function drawWorld() {
-  for (y = randY[Math.floor(Math.random() * Math.floor(randY.length))]; y < 400; y += 50) {
+  for (y = randY[Math.floor(Math.random() * Math.floor(randY.length))]; y < 500; y += 50) {
    new sq(x, y, 50, "#000");
   }
  }
