1326c1d838e4183ec57559fb9df1b8452cef154f
Ethan <48872919+egrayum@users.noreply.github.com>
2020-05-21 17:05:04 -0600

    Update testGame.js

--- docs/testGame.js
+++ docs/testGame.js
@@ -1,19 +1,15 @@
 var c = document.getElementById("gameCanvas");
 var ctx = c.getContext("2d");
 var i;
-var x = 100;
-var y = 100;
+var x;
+var y;
 var world = [];
 var randY = [100, 150, 200];
 function init() {
  c.width = 800;
  c.height = 600;
- ctx.fillStyle = "#666";
+ ctx.fillStyle = "#05aofb";
  ctx.fillRect(0, 0, c.width, c.height);
- ctx.strokeStyle = "#000";
- ctx.lineWidth = 10;
- ctx.moveTo(50, 100);
- ctx.lineTo(750, 100);
  drawWorld();
 }
 function sq(X, Y, Length, Color) {
@@ -21,7 +17,9 @@ function sq(X, Y, Length, Color) {
  ctx.fillRect(X, Y, Length, Length);
 }
 function drawWorld() {
+ for (x = 50; x < 750; x += 50) {
   for (y = randY[Math.floor(Math.random() * Math.floor(randY.length))]; y < 500; y += 50) {
    new sq(x, y, 50, "#000");
-  }
+  }  
  }
+}
