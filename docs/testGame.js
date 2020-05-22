09703e03bbe77a1497a2f920911d8de716a8327a
Ethan <48872919+egrayum@users.noreply.github.com>
2020-05-21 18:43:25 -0600

    Update testGame.js

--- docs/testGame.js
+++ docs/testGame.js
@@ -6,26 +6,59 @@ var y;
 var world = [];
 var randY = [200, 250, 300];
 var prevDepth;
+var moveDist = 0;
+var i;
+var j;
+var drawingX;
+var drawingY;
+var drawingColor;
 function init() {
  c.width = 800;
  c.height = 600;
  ctx.fillStyle = "#05a0fb";
  ctx.fillRect(0, 0, c.width, c.height);
- drawWorld();
+ makeWorld();
+ window.setInterval(drawWorld, 25);
 }
 function block(X, Y, Length, Color) {
  ctx.fillStyle = Color;
  ctx.fillRect(X, Y, Length, Length);
 }
-function drawWorld() {
+function makeWorld() {
  y = randY[Math.floor(Math.random() * Math.floor(randY.length))];
  prevDepth = y;
  for (x = 0; x < 800; x += 50) {
   y = prevDepth + ((Math.floor(Math.random() * Math.floor(3)) - 1) * 50);
   new block(x, y, 50, "#179d06");
   prevDepth = y;
+  world.push[x, y, "#179d06"];
   for (y = y + 50; y < 600; y += 50) {
    new block(x, y, 50, "#7c4f27");
+   world.push[x, y, "#7c4f27"];
   }  
+ }
+}
+function drawWorld() {
+ ctx.fillStyle = "#05a0fb";
+ ctx.fillRect(0, 0, c.width, c.height);
+ document.addEventListener("keydown", function(event) {
+  if (event.keyCode == 38) {
+   moveDist = 5;  
+  } else if (event.keyCode == 40) {
+   moveDist = -5; 
+  }
+ })
+ document.addEventListener("keyup", function(event) {
+  if (event.keyCode == 38) {
+   moveDist = 0;  
+  } else if (event.keyCode == 40) {
+   moveDist = 0; 
+  }
+ })
+ for (i = 0; i < world.length; i++) {
+  drawingX = world[i][0];
+  drawingY = world[i][1];
+  drawingColor = world[i][2];
+  new block(drawingX + moveDist, drawingY + moveDist, 50, drawingColor);
  }
 }
