1bbe14bea34401792990925e25e8ce1618788948
Ethan <48872919+egrayum@users.noreply.github.com>
2020-06-05 10:29:03 -0600

    Update testGame.js

--- docs/testGame.js
+++ docs/testGame.js
@@ -36,7 +36,7 @@ function collision() {
  if (275 > drawingY && 375 > drawingX - 1 && 375 < drawingX + 51) {
   touchingWorld = true;
   pYVel = 0;
-  pYVel += 0.0008;
+  pYVel += 0.0012;
  } else {
   touchingWorld = false;
   pYVel -= 0.0004;
@@ -57,7 +57,7 @@ function init() {
  ctx.fillStyle = "#05a0fb";
  ctx.fillRect(0, 0, c.width, c.height);
  makeWorld();
- alert("version 0.021");
+ alert("version 0.022");
  window.setInterval(drawWorld, 20);
 }
 // for making new blocks
